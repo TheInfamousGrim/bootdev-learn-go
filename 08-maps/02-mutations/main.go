05c741902417baeeb44000a23c1c93b2c77a6827
George Fincher <george@grimfunky.dev>
2024-06-03 20:18:36 +0100

    Feat ✨: Complete mutations exercise

---
+++ 08-maps/02-mutations/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "errors"
+
+func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
+	user, ok := users[name]
+	if ok == false {
+		return false, errors.New("not found")
+	}
+
+	if user.scheduledForDeletion {
+		delete(users, name)
+		return true, nil
+	} else {
+		return false, nil
+	}
+}
+
+type user struct {
+	name                 string
+	number               int
+	scheduledForDeletion bool
+}
